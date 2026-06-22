7e760dd055658bf3d0db1e051aa7cb16c27783bb
Fk4111 <khanfaiyaz359@gmail.com>
2026-06-22 14:46:50 +0530

    Vercel deployment issue resolve

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://your-app-name.vercel.app"
+    "https://equity-backtester.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
